ef241d21b9ff37823192e2f54db2e84328cad8e3
adityarief <adttgnzz2233@gmail.com>
2026-01-09 08:50:52 +0700

    Add files via upload

--- direct/config.php
+++ direct/config.php
@@ -1,1 +1,16 @@
+<?php
+$host = "localhost";
+$db   = "catatan_kepala_toko"; // HARUS SAMA PERSIS
+$user = "root";
+$pass = "";
 
+try {
+    $conn = new PDO(
+        "mysql:host=$host;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass
+    );
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi database gagal: " . $e->getMessage());
+}
