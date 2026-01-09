99f02ccaeb0cdbf0eaae5591792efef35e471d9e
adityarief <adttgnzz2233@gmail.com>
2026-01-09 15:55:36 +0700

    Update header.php

--- modules/header.php
+++ modules/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? $pageTitle : 'Dashboard'; ?> - Catatan Kepala Toko</title>
+    <link rel="icon" type="image/x-icon" href="dist/img/icon.ico">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
@@ -33,5 +34,6 @@
     <?php if(isset($cssFile)): ?>
         <link rel="stylesheet" href="dist/css/<?php echo $cssFile; ?>">
     <?php endif; ?>
+
 
 </head>
