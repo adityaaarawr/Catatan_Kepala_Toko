3d25cc5b25f6c45c6557a690d903e6dfd78d2cb3
adityarief <adttgnzz2233@gmail.com>
2026-01-09 08:45:02 +0700

    Add files via upload

--- modules/footer.php
+++ modules/footer.php
@@ -14,5 +14,18 @@
         <?php endif; ?>
     <?php endif; ?>
 
+    <?php if(isset($pageTitle) && $pageTitle == 'Report'): ?>
+        <!-- JQUERY (WAJIB PALING ATAS) -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+         <!-- SELECT2 -->
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <?php endif; ?>
+
+    <?php if(isset($pageTitle) && $pageTitle == 'User Management'): ?>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <?php endif; ?>
+
 </body>
 </html>
--- modules/header.php
+++ modules/header.php
@@ -13,9 +13,20 @@
         <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
     <?php endif; ?>
 
+    <?php if(isset($pageTitle) && $pageTitle == 'Report'): ?>
+        <!-- SELECT2 -->
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/> 
+    <?php endif; ?>
+
     <?php if(isset($pageTitle) && $pageTitle == 'Master'): ?>
         <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
         <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
+    <?php endif; ?>
+
+    <?php if(isset($pageTitle) && $pageTitle == 'User Management'): ?>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.dataTables.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />   
     <?php endif; ?>
 
     <link rel="stylesheet" href="dist/css/sidebar.css">
