35c151fc4ad33e9ce5dd2cd21c7edc2099665be4
Tjut Karina Aisyairah <164826040+Tjutkarina@users.noreply.github.com>
2026-01-28 01:32:22 +0700

    Add files via upload

--- dist/js/report.js
+++ dist/js/report.js
@@ -51,4 +51,30 @@ $(document).ready(function () {
         if (isAuto) return;
         filterKaryawan();
     });
+
+    // Script untuk Toggle Detail di Mobile
+$(document).on('click', '.toggle-detail', function() {
+    const $tr = $(this).closest('tr');
+    $tr.toggleClass('show-detail');
+    
+    // Ubah teks tombol
+    if ($tr.hasClass('show-detail')) {
+        $(this).text('TUTUP DETAIL');
+    } else {
+        $(this).text('LIHAT DETAIL');
+    }
 });
+});
+
+function toggleMobileFilter() {
+    const filterGrid = document.getElementById('filterGrid');
+    const triggerBtn = document.querySelector('.mobile-filter-trigger');
+    
+    if (filterGrid.classList.contains('show-mobile')) {
+        filterGrid.classList.remove('show-mobile');
+        triggerBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor"><path d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z"/></svg> FILTER DATA';
+    } else {
+        filterGrid.classList.add('show-mobile');
+        triggerBtn.innerHTML = 'TUTUP FILTER';
+    }
+}
