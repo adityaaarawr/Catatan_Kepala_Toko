4f5e04234ecea998b7bed94db6b1e6090f341ed7
Tjut Karina Aisyairah <164826040+Tjutkarina@users.noreply.github.com>
2026-02-04 14:18:56 +0700

    Add files via upload

--- dist/js/report.js
+++ dist/js/report.js
@@ -160,19 +160,6 @@ $(document).on('click', '.toggle-detail', function() {
     }
 });
 
-  function toggleMobileFilter() {
-    const filterGrid = document.getElementById('filterGrid');
-    const triggerBtn = document.querySelector('.mobile-filter-trigger');
-    
-    if (filterGrid.classList.contains('show-mobile')) {
-        filterGrid.classList.remove('show-mobile');
-        triggerBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor"><path d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z"/></svg> FILTER DATA';
-    } else {
-        filterGrid.classList.add('show-mobile');
-        triggerBtn.innerHTML = 'TUTUP FILTER';
-    }
-}
-
 /* ============================================================
        🔟 SIDEBAR TOGGLE (SINKRON DENGAN SIDEBAR.JS)
     ============================================================ */
@@ -192,10 +179,32 @@ $(document).on('click', '.toggle-detail', function() {
         // Event klik yang sinkron
         toggleSidebarBtn.addEventListener("click", function() {
             // Kita biarkan sidebar.js bekerja, tapi kita tambahkan fungsi simpan status
-            setTimeout(() => {
-                const isHidden = sidebar.classList.contains("hide");
-                localStorage.setItem("sidebarStatus", isHidden ? "true" : "false");
-            }, 50); 
+             setTimeout(() => {
+        const isHidden = sidebar.classList.contains("hide");
+
+        // 🔥 INI YANG KURANG
+        if (mainContent) {
+            mainContent.classList.toggle('sidebar-collapsed', isHidden);
+        }
+
+        localStorage.setItem(
+            "sidebarStatus",
+            isHidden ? "true" : "false"
+        );
+    }, 50);
         });
     }
 });
+
+ function toggleMobileFilter() {
+    const filterGrid = document.getElementById('filterGrid');
+    const triggerBtn = document.querySelector('.mobile-filter-trigger');
+    
+    if (filterGrid.classList.contains('show-mobile')) {
+        filterGrid.classList.remove('show-mobile');
+        triggerBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor"><path d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z"/></svg> FILTER DATA';
+    } else {
+        filterGrid.classList.add('show-mobile');
+        triggerBtn.innerHTML = 'TUTUP FILTER';
+    }
+}
